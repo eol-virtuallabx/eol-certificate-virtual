5cdb4f1ef4eda4b373b90fd3817a96faade3dedc
Constanza Altamirano <cons.altamirano@gmail.com>
2021-08-06 09:26:54 -0400

    Update setup name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="eol-certificate-virtual",
+    name="eol_certificate_virtual",
     version="0.0.1",
     author="constanzaaltamirano",
     author_email="constanzaaltamirano@uchile.cl",
